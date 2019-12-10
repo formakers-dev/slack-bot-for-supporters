14d6a4caf104ef8809ff8f9bdee7711a2e3236a1
Jake Kim <copyx00@gmail.com>
2019-12-10 23:44:47 +0900

    feat: RTM 팀 가입 이벤트 리스너 추가
    https://trello.com/c/IlAOyL8B

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 const config = {
     slackApiToken: process.env.SLACK_API_TOKEN,
+    messages: {
+        greeting: process.env.SLACK_MESSAGE_GREETING
+    }
 };
 
 module.exports = config;
--- controller/rtm.js
+++ controller/rtm.js
@@ -1,12 +1,22 @@
-const {RTMClient} = require('@slack/client');
+const {RTMClient} = require('@slack/rtm-api');
+const {WebClient} = require('@slack/web-api');
 const config = require('../config');
 
 const botApiToken = config.slackApiToken;
 const rtm = new RTMClient(botApiToken);
+const web = new WebClient(botApiToken);
 
 rtm.on('authenticated', rtmStartData => {
     console.log('Logged in as ' + rtmStartData.self.name + ' of them '
         + rtmStartData.team.name + ', but not yet connected to a channel');
+});
+
+rtm.on('team_join', event => {
+    web.im.open({user: event.user})
+        .then(result => {
+            rtm.sendMessage(config.messages.greeting, result.channel.id);
+        })
+        .catch(err => console.log(err));
 });
 
 const listen = (req, res) => {
