0602adcd15e7be7387be87117ef86a9122af4601
yenarue <yenarue@gmail.com>
2020-02-23 12:38:02 +0900

    chore: 채널정보 개발계, 운영계 분리
    https://trello.com/c/6jIh6eVY

--- data/channels.js
+++ data/channels.js
@@ -1,26 +1,46 @@
-module.exports = [
-    {
-        id: "CRH1WHLNS",
-        name: "general",
-        type: "public",
-    },
-    {
-        id: "CRF1LKG9K",
-        name: "game_issues",
-        type: "public",
-    },
-    {
-        id: "CRJH41YLD",
-        name: "fomes-issues",
-        type: "public",
-    },
-    {
-        id: "CR6SXHLNM",
-        name: "supporters_2nd",
-        type: "private",
-        active: {
-            startDate: new Date("2019-12-15T15:00:00.000Z"),
-            endDate: new Date("2020-02-23T14:59:99.999Z"),
+const channels = {
+    development: [
+        {
+            id: "CRJDTED5Z",
+            name: "general",
+            type: "public",
+        },
+        {
+            id: "C919H4HEY",
+            name: "dev",
+            type: "private",
+            active: {
+                startDate: new Date("2019-12-15T15:00:00Z"),
+                endDate: new Date("2029-12-15T15:00:00Z"),
+            }
         }
-    }
-];
+    ],
+    production: [
+        {
+            id: "CRH1WHLNS",
+            name: "general",
+            type: "public",
+        },
+        {
+            id: "CRF1LKG9K",
+            name: "game_issues",
+            type: "public",
+        },
+        {
+            id: "CRJH41YLD",
+            name: "fomes-issues",
+            type: "public",
+        },
+        {
+            id: "CR6SXHLNM",
+            name: "supporters_2nd",
+            type: "private",
+            active: {
+                startDate: new Date("2019-12-15T15:00:00Z"),
+                endDate: new Date("2020-02-23T14:59:99Z"),
+            }
+        }
+    ],
+};
+
+module.exports = channels[process.env.NODE_ENV];
