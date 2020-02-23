f34143253a501fe06b1c23f8690335a81da86391
yenarue <yenarue@gmail.com>
2020-02-23 12:16:02 +0900

    chore: channel 정보 구체화
    https://trello.com/c/6jIh6eVY

--- data/channels.js
+++ data/channels.js
@@ -1,8 +1,26 @@
 module.exports = [
     {
-        general: "CRH1WHLNS",
-        game_issues: "CRF1LKG9K",
-        fomes_issues: "CRJH41YLD",
-        supporters_2nd: "CR6SXHLNM",
+        id: "CRH1WHLNS",
+        name: "general",
+        type: "public",
+    },
+    {
+        id: "CRF1LKG9K",
+        name: "game_issues",
+        type: "public",
+    },
+    {
+        id: "CRJH41YLD",
+        name: "fomes-issues",
+        type: "public",
+    },
+    {
+        id: "CR6SXHLNM",
+        name: "supporters_2nd",
+        type: "private",
+        active: {
+            startDate: new Date("2019-12-15T15:00:00.000Z"),
+            endDate: new Date("2020-02-23T14:59:99.999Z"),
+        }
     }
 ];
